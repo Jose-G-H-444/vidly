5894921d65047d33a53f599bfc816fcaefec12fe
Jose G <jose.gallardo@gttusa.net>
2021-11-12 11:57:47 -0800

    Add genres to mongodb

--- routes/genres.js
+++ routes/genres.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const debug = require('debug')('app:base');
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 // Required middleware
 router.use(express.json());
@@ -19,14 +20,25 @@ const genreSchema = new mongoose.Schema({
         minlength: 4,
         maxlength: 20,
         lowercase: true
-    }
+    },
+    datefounded: Date
 });
+const Genre = mongoose.model('Genre', genreSchema);
 
-let genres = [
-    { id: 1, name: 'Horror'},
-    { id: 2, name: 'RomCom'},
-    { id: 3, name: 'Comedy'}
-];
+// Add genres to database
+async function addGenres() {
+    const genreData = fs.readFileSync('routes/genres.json');
+    let genres = JSON.parse(genreData);
+    courses = await Genre.insertMany(genres);
+    debug(genres);
+}
+addGenres();
+
+// let genres = [
+//     { id: 1, name: 'Horror'},
+//     { id: 2, name: 'RomCom'},
+//     { id: 3, name: 'Comedy'}
+// ];
 
 router.get('/', (req, res) => {
     res.send(genres);
